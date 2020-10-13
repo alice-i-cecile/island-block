12609ac9b84cb7a0c9e2df8e1cf2fbb6482eaa71
Alice Cecile <alice.i.cecile@gmail.com>
2020-10-13 16:46:43 -0400

    Place tiles normally

--- IslandBlock_0.0.0/data.lua
+++ IslandBlock_0.0.0/data.lua
@@ -5,7 +5,3 @@ require "prototypes/technology"
 local lib = require "lib"
 
 table.insert(data.raw.character.character.crafting_categories, "crafting-handonly")
-
-for _,v in pairs(data.raw.tile) do
-  v.autoplace = nil
-end
